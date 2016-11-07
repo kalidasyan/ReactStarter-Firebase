a4431191d51b873bfea267bb0d369bce402c3a6b
tyan <tyan@linkedin.com>
2016-11-06 17:38:38 -0800

    Use a different ReactFire version before the tutorial's version is not working - lecture 27

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,23 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var rootUrl = 'https://test-project-1-fe948.firebaseio.com/';
+
+var config = {
+  databaseURL: rootUrl
+};
+
+Firebase.initializeApp(config);
 
 var Hello = React.createClass({
+  mixins: [ReactFire ],
+  componentWillMount: function() {
+    var ref = Firebase.database().ref("items");
+    this.bindAsObject(ref, "items");
+  },
   render: function() {
     return <h1 className="red">
-      Hello! World
+      Hello!
     </h1>
   }
 });
