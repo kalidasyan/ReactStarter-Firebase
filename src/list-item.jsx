92890c8896e0c81c465826433174b21f3a7ca3e3
tyan <tyan@linkedin.com>
2016-11-07 18:16:08 -0800

    Updating data to remote database - lecture 34

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,15 +1,26 @@
 var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://test-project-1-fe948.firebaseio.com/';
+
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
+  },
+  componentWillMount: function(){
+    this.ref = Firebase.database().ref("items/" + this.props.item.key);
   },
   render: function() {
     return <div className="input-group">
       <span className="input-group-addon">
-        <input type="checkbox" />
+        <input
+          checked={this.state.done}
+          onChange={this.handleDoneChange}
+          type="checkbox"
+          />
       </span>
       <input type="text"
         className="form-control"
@@ -21,5 +32,10 @@ module.exports = React.createClass({
         </button>
       </span>
     </div>
+  },
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked};
+    this.setState(update);
+    this.ref.update(update);
   }
 });
